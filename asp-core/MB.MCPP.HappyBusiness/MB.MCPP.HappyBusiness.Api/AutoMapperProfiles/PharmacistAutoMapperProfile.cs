917c3e79be6e17f97c0afdf5f798c15144ebd136
Idrees Haddad <gelnox@live.com>
2022-08-09 18:22:45 +0300

    Finihed Pharmacist controller

---
+++ asp-core/MB.MCPP.HappyBusiness/MB.MCPP.HappyBusiness.Api/AutoMapperProfiles/PharmacistAutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MB.MCPP.HappyBusiness.Dtos.Pharmacists;
+using MB.MCPP.HappyBusiness.Entities;
+
+namespace MB.MCPP.HappyBusiness.Api.AutoMapperProfiles
+{
+    public class PharmacistAutoMapperProfile : Profile
+    {
+        public PharmacistAutoMapperProfile()
+        {
+            CreateMap<PharmacistDto, Pharmacist>();
+            CreateMap<Pharmacist, PharmacistListDto>();
+            CreateMap<Pharmacist, PharmacistDetailsDto>();
+        }
+    }
+}
---
+++ asp-core/MB.MCPP.HappyBusiness/MB.MCPP.HappyBusiness.Api/Controllers/PharmacistsController.cs
@@ -0,0 +1,108 @@
+﻿using AutoMapper;
+using MB.MCPP.HappyBusiness.Dtos.Pharmacists;
+using MB.MCPP.HappyBusiness.Entities;
+using MB.MCPP.HappyBusiness.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MB.MCPP.HappyBusiness.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PharmacistsController : ControllerBase
+    {
+        #region Data And Constructors
+
+        private readonly IMapper _mapper;
+        private readonly ApplicationDbContext _context;
+        public PharmacistsController(ApplicationDbContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+        #endregion
+
+        #region Services
+
+        [HttpGet]
+        public async Task<List<PharmacistListDto>> GetPharmacistsList()
+        {
+            var pharmacists = await _context
+                                    .Pharmacists
+                                    .ToListAsync();
+
+            var pharmacistDtos = _mapper.Map<List<PharmacistListDto>>(pharmacists);
+            return pharmacistDtos;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PharmacistDetailsDto>> GetPharmacistById(int id)
+        {
+            var pharmacist = await _context.Pharmacists.FindAsync(id);
+
+            if(pharmacist == null)
+            {
+                return NotFound();
+            }
+
+            var pharmacistDto = _mapper.Map<PharmacistDetailsDto>(pharmacist);
+
+            return pharmacistDto;
+        }
+
+        [HttpPost]
+        public async Task<int> CreatePharmacist([FromBody] PharmacistDto pharmacistDto)
+        {
+            var pharmacist = _mapper.Map<Pharmacist>(pharmacistDto);
+
+            _context.Add(pharmacist);
+            await _context.SaveChangesAsync();
+
+            return pharmacist.Id;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditPharmacist(int id, [FromBody] PharmacistDto pharmacistDto)
+        {
+            if(id != pharmacistDto.Id)
+            {
+                return BadRequest();
+            }
+
+            var pharmacist = _mapper.Map<Pharmacist>(pharmacistDto);
+
+            try
+            {
+                _context.Update(pharmacist);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            { 
+                throw new DbUpdateException($"Couldn't update Pharmacist Id=[{id}]", ex);
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletePharmacistById(int id)
+        {
+            var pharmacist = await _context.Pharmacists.FindAsync(id);
+
+            if (pharmacist == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(pharmacist);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+        #endregion    
+
+        #region Private Methods
+
+        #endregion
+    }
+}
